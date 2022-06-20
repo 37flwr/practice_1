76cac8c060574914d91760a79157d09dae578ad8
37flwr <nesterishinyarik@gmail.com>
2022-06-20 11:36:34 +0300

    Add basic scss structure
    Add basic scss structure
    
    WIP

--- src/pages/HomePage/components/Team/index.js
+++ src/pages/HomePage/components/Team/index.js
@@ -1,8 +1,8 @@
-import './styles.scss';
-import axios from 'axios';
 import { useEffect, useMemo, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { getUsers } from '../../../../store/users/actions';
+
+import './styles.scss';
 
 const Team = () => {
     const dispatch = useDispatch()
@@ -17,51 +17,57 @@ const Team = () => {
 
     console.log(memoizedUsers);
 
-  return (
-    <section className="team-part">
-        {loading ? 
-            <div>Loading...</div>
-        :
-        memoizedUsers.users.length > 0 ?
-            <>
-                {memoizedUsers.users.map(({ id, photo, name, position, email, phone }) => (
-                    <div className="team-item">
-                        <div className="team-item-container" key={id}>
-                            <img src={photo} alt="" className="team-item-img" />
-                            <span className="team-item-heading">
-                                {name}
-                            </span>
-                            <div>
-                                <span>
-                                    {position}
-                                </span>
-                                <span>
-                                    {email}
-                                </span>
-                                <span>
-                                    {phone}
-                                </span>
-                            </div>
+    return (
+        <section className="team-part">
+            {loading ? 
+                <div>Loading...</div>
+            :
+            memoizedUsers.users.length > 0 ?
+                <>
+                    <div className="team-container">
+                        <div className="team-items-grid">
+                            {memoizedUsers.users.map(({ id, photo, name, position, email, phone }) => (
+                                <div className="team-item">
+                                    <div className="team-item-container" key={id}>
+                                        <img src={photo} alt="" className="team-item-img" />
+                                        <div className="team-item-heading nunito">
+                                            <span>
+                                                {name}
+                                            </span>
+                                        </div>
+                                        <div className="team-item-details">
+                                            <span className="team-item-text">
+                                                {position}
+                                            </span>
+                                            <span className="team-item-text">
+                                                {email}
+                                            </span>
+                                            <span className="team-item-text">
+                                                {phone}
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div>
+                            ))}
                         </div>
+                        {memoizedUsers.total_pages > page+1 ?
+                            <button onClick={() => setPage(page+1)}>
+                                Upgrade
+                            </button>
+                        :
+                            <button>
+                                Exceeded
+                            </button>
+                        }
                     </div>
-                ))}
-                {memoizedUsers.total_pages > page+1 ?
-                    <button onClick={() => setPage(page+1)}>
-                        Upgrade
-                    </button>
-                :
-                    <button>
-                        Exceeded
-                    </button>
-                }
-            </>
-        :
-            <div>
-                No users found
-            </div>
-        }
-    </section>
-  )
+                </>
+            :
+                <div>
+                    No users found
+                </div>
+            }
+        </section>
+    )
 }
 
 export default Team
